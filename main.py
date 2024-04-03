361e9134a03915e9ea689690c0e37bc12c3160f7
martabritala <53839047+martabritala@users.noreply.github.com>
2024-04-03 14:27:40 +0300

    03.04. stundas darbs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from dati import dabut_rindinas
 
 app = Flask(__name__)
 
@@ -14,7 +15,12 @@ def te():
 def saraksts():
     saraksts = ["Anna", "Katls", "Kartupelis"]
     bildes = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcCG4898FTkK5IUBMpLleBHtjK3jUILS3YsjCFfLr56g&s","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtv-3G97RDAt-sgNqxhaEB4f2SDwVxJaOq7JOY0D_7zA&s","https://www.darzaabc.lv/public/assets/images/products/Agrimatco/kartupe%C4%BCi/kartupeli-monalisa-dzeltenie-seklas-kartupelu-stadamais-materials.jpg"]
-    kopejais_saraksts = [["Anna","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcCG4898FTkK5IUBMpLleBHtjK3jUILS3YsjCFfLr56g&s"], ["Katls", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtv-3G97RDAt-sgNqxhaEB4f2SDwVxJaOq7JOY0D_7zA&s"], ["Kartupelis", "https://www.darzaabc.lv/public/assets/images/products/Agrimatco/kartupe%C4%BCi/kartupeli-monalisa-dzeltenie-seklas-kartupelu-stadamais-materials.jpg"], ["Mašīna", "https://lh6.googleusercontent.com/Bs8IK7AzA7HDKeel06gttMMDPDoPNzBQdxUTgoKnnFpWhw8tDn8OUWeaaOZeIDnuujmOiEhPBlbDbkjSHKZb-EWByLlqgJCmkF-V-cRau3hEyNqGD-uWqrXEXxnSpvgxj8300Al7"]]
+    kopejais_saraksts = []
+    faila_rindas = dabut_rindinas()
+    for rinda in faila_rindas:
+        elements = rinda.split(", ")
+        kopejais_saraksts.append(elements)
+
     return render_template("saraksts.html", vardi = saraksts, bildes = bildes, garums = len(saraksts), visi = kopejais_saraksts)
 
 
